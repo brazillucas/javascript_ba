f4dbe0855b51c24140ea159578899da1b609ff02
brazillucas <l.cardoso64@hotmail.com>
2022-10-17 11:31:57 -0300

    Introdução do Quizz, faltando o script

---
+++ 21_PROJETO_QUIZZ/js/script.js
@@ -0,0 +1,102 @@
+// Declaração de Variáveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQUestion = 0;
+
+//  Perguntas
+const questions = [
+        {
+          "question": "PHP foi desenvolvido para qual fim?",
+          "answers": [
+            {
+              "answer": "back-end",
+              "correct": true
+            },
+            {
+              "answer": "front-end",
+              "correct": false
+            },
+            {
+              "answer": "Sistema operacional",
+              "correct": false
+            },
+            {
+              "answer": "Banco de dados",
+              "correct": false
+            },
+          ]
+        },
+        {
+          "question": "Uma forma de declarar variável em JavaScript:",
+          "answers": [
+            {
+              "answer": "$var",
+              "correct": false
+            },
+            {
+              "answer": "var",
+              "correct": true
+            },
+            {
+              "answer": "@var",
+              "correct": false
+            },
+            {
+              "answer": "#let",
+              "correct": false
+            },
+          ]
+        },
+        {
+          "question": "Qual o seletor de id no CSS?",
+          "answers": [
+            {
+              "answer": "#",
+              "correct": true
+            },
+            {
+              "answer": ".",
+              "correct": false
+            },
+            {
+              "answer": "@",
+              "correct": false
+            },
+            {
+              "answer": "/",
+              "correct": false
+            },
+          ]
+        },
+]
+
+// Substituição do quizz para a primeira pergunta
+function init() {
+    // Cirar primeira pergunta
+    console.log("iniciou");
+    createQuestion(0);
+}
+
+// Cria uma pergunta
+function createQuestion(i) {
+    // Limpar a questão anterior
+    const oldButtons = answersBox.querySelectorAll("button");
+
+    oldButtons.forEach(function(btn) {
+        btn.remove();
+    });
+
+    // Alterar texto da pergunta
+    const questionText = question.querySelector("#question-text");
+    const questionNumber = question.querySelector("#question-number");
+
+    questionText.textContent = questions[1].question;
+    questionNumber.textContent = i + 1;
+}
+
+// Iniciando o quizz
+init();
